7478ddaf76732c307deb628a8798ec19a9e5cab8
onlyJho <jhomaira.perezu@unl.edu.ec>
2019-06-11 16:42:34 -0500

    Habilitado la modificación de categoría

--- controllers/CategoriaController.js
+++ controllers/CategoriaController.js
@@ -4,21 +4,33 @@ var Categoria = models.categoria;
 const uuidv4 = require('uuid/v4');
 class CategoriaController {
     guardarCategoria(req, res) {
-        Categoria.create({
-            nombre: req.body.nombre,
-            external_id: uuidv4()
+        if (req.body.external == 0) {
+            Categoria.create({
+                nombre: req.body.nombre,
+                external_id: uuidv4()
 
-        }).then(function (newCategoria, created) {
-            console.log(newCategoria);
-            if (newCategoria) {
+            }).then(function (newCategoria, created) {
+                console.log(newCategoria);
+                if (newCategoria) {
+                    res.redirect('/categorias');
+                    console.log('Se guardo categoria............');
+                    req.flash('info', 'Creación exitosa', false);
+                }
+            });
+        } else {
+            Categoria.update({
+                nombre: req.body.nombre
+            }, {where: {external_id: req.body.external}}).then(function (updatedcategoria, created) {
                 res.redirect('/categorias');
-                console.log('Se guardo categoria............');
-                req.flash('info', 'Creación exitosa', false);
-            }
-        });
+                if (updatedcategoria) {
+                    req.flash('info', 'Modificación éxitosa', false);
+                    console.log('Se modifico categoria............');
+                }
+            });
+        }
     }
-    
-    listarCategoria(req, res){
+
+    listarCategoria(req, res) {
         Categoria.findAll({}).then(function (listaCategoria) {
             console.log(listaCategoria);
             res.render('categoria', {title: 'Categorias', categoria: listaCategoria});
