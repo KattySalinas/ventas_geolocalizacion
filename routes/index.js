4e90077b7c3c67219263afb33f9d2dbbb54e5d30
onlyJho <jhomaira.perezu@unl.edu.ec>
2019-07-16 09:28:31 -0500

    upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer');
+var upload = multer ({dest: './uploads/'});
+
+var fs = require ('fs');
+
 var cuenta = require('../controllers/CuentaController');
 var CuentaController = new cuenta;
 
@@ -37,7 +42,20 @@ router.post('/registrarComerciante', CuentaController.guardarComerciante);
 router.post('/registrarCategoria', CategoriaController.guardarCategoria);
 router.get('/categorias',CategoriaController.listarCategoria);
 
-router.post('/registrarProducto', ProductoController.guardarProducto);
+//router.post('/registrarProducto', ProductoController.guardarProducto);
 router.get('/productos',ProductoController.listarProducto);
+
+router.post('/registrarProducto', upload.array('base[]', 6), function(req, res, next) {
+    for(var x=0;x<req.files.length;x++) {
+       //copiamos el archivo a la carpeta definitiva de fotos
+       fs.createReadStream('./uploads/'+req.files[x].filename).pipe(fs.createWriteStream('./public/fotos/'+req.files[x].originalname)); 
+       //borramos el archivo temporal creado
+       fs.unlink('./uploads/'+req.files[x].filename); 
+    }  
+    var pagina='<!doctype html><html><head></head><body>'+
+               '<p>Se subieron las fotos</p>'+
+               '<br><a href="/">Retornar</a></body></html>';
+      res.send(pagina);        
+});
 
 module.exports = router;
